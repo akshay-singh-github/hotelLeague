e514796a94c5cb9b359b6f6ebb02aa3b9c9a0ac1
akshaysingh1520 <akshaysingh1520@gmail.com>
2017-08-10 23:20:26 -0400

    update

--- server-side-implementation/services/google.service.server.js
+++ server-side-implementation/services/google.service.server.js
@@ -8,13 +8,14 @@ module.exports=function (app,model) {
     app.post("/api/hotel/details" , getHotelDetails);
     app.get("/api/google/apiKey" , getApiKey);
     
-    var GOOGLE_API_KEY = "AIzaSyDGxY0mVTT3OXdOYSwf4UZtSr_X1e3PkoU";
+    var GOOGLE_API_KEY = "AIzaSyCs1cUSABA5pOEvshw8nIcDs_stsHQC9eY";
 
     /*AIzaSyAeildH4NJUWZsBHSCYZPdUoE89O0MFDys*/ //static khan
     /*AIzaSyA2Bhea9SgEMLUpWGOHViSBv8iEpYQky9Y*/
     /*AIzaSyC5BexhekQh3Vn7H6xr4727j0bQkkd8R24*/ //khan
     //DJIHAnBlzcy2F39QY4jLsNwL
     /*AIzaSyDGxY0mVTT3OXdOYSwf4UZtSr_X1e3PkoU*/ /*dummy*/
+    /*AIzaSyCs1cUSABA5pOEvshw8nIcDs_stsHQC9eY*/ /*dummy2*/
 
     function getApiKey(req, res) {
 
